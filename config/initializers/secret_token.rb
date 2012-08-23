64ddd36009f7958c4462a04f9d51a58a63ee384e
Sean Lowen <stonesfan101@hotmail.com>
2012-08-23 11:53:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '01df1b51d4eba190c2416179222bc5710098a38189f511b89a2c1769c4f49b2ea4765ada90cb5838bbd792c8b94ad48ed0fe35487fae2f7dd16d2334ff4c738c'
